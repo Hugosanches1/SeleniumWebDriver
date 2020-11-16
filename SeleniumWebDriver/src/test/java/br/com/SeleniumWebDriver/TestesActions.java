5f60416c374a5da8259cfed09b1996e191d8b1c6
Hugosanches <Hugosanches87@gmail.com>
2020-11-15 22:31:53 -0300

    Otimização dos imports

--- SeleniumWebDriver/src/test/java/br/com/SeleniumWebDriver/TestesActions.java
+++ SeleniumWebDriver/src/test/java/br/com/SeleniumWebDriver/TestesActions.java
@@ -2,12 +2,9 @@ package br.com.SeleniumWebDriver;
 
 import java.util.concurrent.TimeUnit;
 
-import org.junit.rules.Timeout;
-import org.omg.CORBA.TIMEOUT;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriver.Timeouts;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
